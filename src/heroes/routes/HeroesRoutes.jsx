f19971440af687ff47527100792caeb731e1f84f
Alex Moreano <alxstaleycobain@hotmail.com>
2022-11-22 17:20:32 -0500

    Added routes and router to the project, some components also added

---
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Navigate, Route, Routes } from "react-router-dom"
+import { Navbar } from '../../ui/components'
+import { MarvelPage, DcPage, Hero, Search } from '../pages'
+
+export const HeroesRoutes = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className='container'>
+        <Routes>
+            <Route path="marvel" element={<MarvelPage/>}/>
+            <Route path="dc" element={<DcPage/>}/>
+            <Route path="search" element={<Search/>}/>
+            <Route path="hero" element={<Hero/>}/>
+            <Route path="/" element={<Navigate to='/marvel'/>}/>
+        </Routes>
+        </div>
+    </div>
+  )
+}
