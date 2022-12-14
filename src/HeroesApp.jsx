f22a3e04c4756638ab5018e5232164fab65387ee
Alex Moreano <alxstaleycobain@hotmail.com>
2022-12-13 21:08:16 -0500

    Context and reducer added to make login

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,9 +1,10 @@
+import { AuthProvider } from "./auth/context/AuthProvider"
 import { AppRouter } from "./router/AppRouter"
 
 export const HeroesApp = () => {
   return (
-    <div>
+    <AuthProvider>
        <AppRouter/>
-    </div>
+    </AuthProvider>
   )
 }
